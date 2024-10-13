80246920fca41ccbc2dd6b7418c19bdf6c5c63a5
Alberto Valera <153525133+aalbertovalera48@users.noreply.github.com>
2024-10-13 21:39:58 +0200

    Implementación clase WebConfig , Actualizaciones finales clases: SimulaciónController y HomeController

--- src/main/java/Controllers/HomeController.java
+++ src/main/java/Controllers/HomeController.java
@@ -7,6 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @GetMapping("/home")
+    public String home (Model model) {
+        return "home";
+    }
 
     @GetMapping("/login")
     public String login (Model model) {
--- src/main/java/GaltonSpringBoot/SimulacionController.java
+++ src/main/java/GaltonSpringBoot/SimulacionController.java
@@ -1,12 +1,13 @@
 package GaltonSpringBoot;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class SimulacionController {
@@ -18,19 +19,23 @@ public class SimulacionController {
         this.executor = Executors.newFixedThreadPool(4); // Pool de hilos
     }
 
-    @GetMapping("/")
-    public String simulacion(Model model) {
+    @GetMapping("/simulacion")
+    public String simulacion() {
         return "simulacion";
     }
 
     @GetMapping("/simular")
     @ResponseBody
-    public int[] simular(int numBalls) {
+    public int[] simular(@RequestParam int numBalls) {
         for (int i = 0; i < numBalls; i++) {
             executor.execute(new Pelota1(board)); // Ejecutar la caída de cada bola
         }
-
+        executor.shutdown();
+        try {
+            executor.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
         return board.getBins(); // Devolver los resultados
     }
 }
-
