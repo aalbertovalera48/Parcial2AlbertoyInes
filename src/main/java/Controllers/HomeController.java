937ec4e0b208c0eaf9d2df2cf61c664c37180b96
inesgmz <igomeolc@myuax.com>
2024-10-13 20:55:07 +0200

    Actualizacion clase HomeController

--- src/main/java/Controllers/HomeController.java
+++ src/main/java/Controllers/HomeController.java
@@ -1,5 +1,6 @@
 package Controllers;
 
+import Data.RegistroData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +8,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/about")
-    public String about(Model model) {
-        return "about";
+    @GetMapping("/login")
+    public String login (Model model) {
+        return "login";
+    }
+
+    @GetMapping("/registro")
+    public String registro (Model model) {
+        model.addAttribute("registroData", new RegistroData());
+        return "registro";
+    }
+
+    @GetMapping("/simulacion")
+    public String simulacion (Model model) {
+        return "simulacion";
     }
 
 }
